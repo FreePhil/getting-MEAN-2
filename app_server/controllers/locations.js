6ad1fa4994939c544116b89f196be1116874c7ee
Phil@Laverne <phil@ms1.hanlin.com.tw>
2019-06-24 22:20:26 +0800

    try trigger build

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -12,7 +12,7 @@ const renderHomepage = (request, response, body) => {
       {
         title: 'Loc8r - find a place to work with wifi',
         pageHeader: {
-          title: 'Loc8r',
+          title: 'Loc8r1',
           strapline: 'Find places to work with wifi near you!'
         },
         sidebar: "Looking for wifi and a seat? Loc8r helps you find places to work when out and about. Perhaps with coffee, cake or a pint? Let Loc8r help you find the place you're looking for.",
